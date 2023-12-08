69a7dad605f87de60eb7f52815b9d2156f622678
Adrien Davy <adriendavyfx@gmail.com>
2023-12-08 20:58:23 +0100

    prepare pages's routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cgi from "./pages/Cgi";
+import Devweb from "./pages/Devweb";
+import Videos from "./pages/Videos";
+import Contact from "./pages/Contact";
 function App() {
 
   return (
-    <>
-      Hello world
-    </>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cgi" element={<Cgi />} />
+        <Route path="/devweb" element={<Devweb />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
   )
 }
 
