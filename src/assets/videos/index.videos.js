4c948ec2bd47dbcf272ffc651216ecc2a5222a1f
Adrien Davy <adriendavyfx@gmail.com>
2023-12-09 13:11:20 +0100

    video library filled

--- src/assets/videos/index.videos.js
+++ src/assets/videos/index.videos.js
@@ -1,6 +1,14 @@
 import { soft, work, category } from '../keywordList.js';
 import dressedThumb from '@thumbnails/dressed_like_a_naked_soul.png'
 import charlie from '@thumbnails/charlie.png'
+import dantesk from '@thumbnails/dantesk.png'
+import reperkusound from '@thumbnails/reperkusound.png'
+import bassodrome from '@thumbnails/bassodrome.png'
+import sygma from '@thumbnails/sygma.png'
+import vietnam from '@thumbnails/vietnam.png'
+import whitsundays from '@thumbnails/whitsundays.png'
+import greatOceanRoad from '@thumbnails/great_ocean_road.png'
+import arolla from '@thumbnails/arolla.png'
 const videoLibrary = [
   {
     id: 1,
@@ -25,6 +33,114 @@ const videoLibrary = [
     year: 2016,
     thumbnail: charlie,
     video: "https://www.youtube.com/embed/wT5VEkvqCdc"
+  },
+  {
+    id: 3,
+    number: "03",
+    title: "Dantesk",
+    description: "Aftermovie réalisé pour Big Foot Secret Stage à Lyon",
+    software: [soft.premiere, soft.after],
+    work: [work.capta, work.montage, work.fx, work.etalo],
+    category: [category.aftermovie],
+    year: 2015,
+    thumbnail: dantesk,
+    video: "https://www.youtube.com/embed/8rWluVbIlKA"
+  },
+  {
+    id: 4,
+    number: "04",
+    title: "Reperkusound",
+    description: "Aftermovie réalisé pour Mediatone à Lyon",
+    software: [soft.premiere, soft.after],
+    work: [work.capta, work.montage, work.fx, work.etalo],
+    category: [category.aftermovie],
+    year: 2015,
+    thumbnail: reperkusound,
+    video: "https://www.youtube.com/embed/UW9CjI1XZQQ"
+  },
+  {
+    id: 5,
+    number: "05",
+    title: "Bassodrome",
+    description: "Live report réalisé pour le festival du Bassodrome à Grenoble",
+    software: [soft.premiere, soft.after],
+    work: [work.capta, work.montage, work.fx, work.etalo],
+    category: [category.liveReport],
+    year: 2015,
+    thumbnail: bassodrome,
+    video: "https://www.youtube.com/embed/cezSdKJEsiU"
+  },
+  {
+    id: 6,
+    number: "06",
+    title: "Alien Ceremony",
+    description: "Aftermovie réalisé pour l'association de djs Sygma",
+    software: [soft.premiere, soft.after],
+    work: [work.capta, work.montage, work.fx, work.etalo],
+    category: [category.aftermovie],
+    year: 2016,
+    thumbnail: sygma,
+    video: "https://www.youtube.com/embed/YPun_RZc62E"
+  },
+  {
+    id: 7,
+    number: "07",
+    title: "Viêtnam",
+    description: "Vidéo réalisée pour un voyage de 1 mois dans le nord du Viêtnam",
+    software: [soft.premiere, soft.after],
+    work: [work.cadrage, work.montage, work.fx, work.etalo],
+    category: [category.travel],
+    year: 2019,
+    thumbnail: vietnam,
+    video: "https://www.youtube.com/embed/TcGwQQMH4qw"
+  },
+  {
+    id: 8,
+    number: "08",
+    title: "Maroc",
+    description: "Vidéo réalisée pour un voyage dans le sud du Maroc",
+    software: [soft.premiere, soft.after],
+    work: [work.cadrage, work.montage, work.fx, work.etalo],
+    category: [category.travel],
+    year: 2019,
+    thumbnail: vietnam,
+    video: "https://www.youtube.com/embed/TcGwQQMH4qw"
+  },
+  {
+    id: 9,
+    number: "09",
+    title: "Whitsundays",
+    description: "Vidéo réalisée pour un voyage sur la côte Est en Australie",
+    software: [soft.premiere, soft.after],
+    work: [work.drone, work.cadrage, work.montage, work.etalo],
+    category: [category.travel],
+    year: 2019,
+    thumbnail: whitsundays,
+    video: "https://www.youtube.com/embed/XOw0sLW8e0c"
+  },
+  {
+    id: 10,
+    number: "10",
+    title: "Great Ocean Road",
+    description: "Vidéo réalisée pour un voyage sur la côte Est en Australie",
+    software: [soft.premiere, soft.after],
+    work: [work.drone, work.cadrage, work.montage, work.etalo],
+    category: [category.travel],
+    year: 2019,
+    thumbnail: greatOceanRoad,
+    video: "https://www.youtube.com/embed/dULoxNsVhP8"
+  },
+  {
+    id: 11,
+    number: "11",
+    title: "Arolla",
+    description: "Vidéo réalisée quand j'habitais en Suisse",
+    software: [soft.premiere, soft.after],
+    work: [work.drone, work.montage, work.etalo],
+    category: [category.travel],
+    year: 2020,
+    thumbnail: arolla,
+    video: "https://www.youtube.com/embed/Wu8aTQr1xw4"
   },
 ]
 
