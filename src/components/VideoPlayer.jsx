e7c8bb97a1900f70e63e5b821cbc4b6b657d975a
Adrien Davy <adriendavyfx@gmail.com>
2023-12-11 17:32:05 +0100

    add work condition

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -22,26 +22,28 @@ const VideoPlayer = () => {
     setSelectedVideo(video);
   };
 
+
   return (
     <div style={{ background: "black" }}>
       <div style={{ position: "fixed", background: "blue" }}>
         {selectedVideo && (
           <div ref={videoRef}>
-            <p>{selectedVideo.number}</p>
-            <h2>{selectedVideo.title}</h2>
-            <img src={selectedVideo.thumbnail} alt={selectedVideo.title} />
-            <iframe title={selectedVideo.title} src={selectedVideo.video} allowFullScreen />
+            <p>{selectedVideo?.number}</p>
+            <h2>{selectedVideo?.title}</h2>
+            <img src={selectedVideo?.thumbnail} alt={selectedVideo?.title} style={{ opacity: "0", width: "500px", height: "auto" }} />
+            <iframe title={selectedVideo?.title} src={selectedVideo?.video} allowFullScreen />
           </div>
         )}
       </div>
       {libraryToDisplay.map((video) => (
         <div onClick={() => handleClickVideo(video)} key={video.id} style={{ background: "red", margin: "20px 0" }}>
-          <p>{video.number}</p>
-          <h2>{video.title}</h2>
-          <h3>{video.year} </h3>
-          <p>{video.description}</p>
-          <p>{video.software.join(" | ")} </p>
-          <img src={video.thumbnail} alt={video.title} />
+          <p>{video?.number}</p>
+          <h2>{video?.title}</h2>
+          <h4>{video?.year} </h4>
+          <p>{video?.description}</p>
+          <h3>{video?.work ? video.work.join(" | ") : null} </h3>
+          <h3>{video?.software.join(" | ")} </h3>
+          <img src={video?.thumbnail} alt={video?.title} style={{ width: "500px", height: "auto" }} />
         </div>
       )
       )}
