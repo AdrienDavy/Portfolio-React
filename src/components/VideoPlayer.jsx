506fc74956beb4bcf76ad53d2f02b85b3f5ce743
Adrien Davy <adriendavyfx@gmail.com>
2023-12-11 16:24:42 +0100

    function video player location display if cgi or video done

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import cgiLibrary from "@cgi/index.cgi";
+import videoLibrary from "@videos/index.videos";
+import { useLocation } from "react-router-dom";
 
 const VideoPlayer = () => {
-  const [selectedVideo, setSelectedVideo] = useState(cgiLibrary[0]);
+
+  const location = useLocation();
+  const isCgiPage = location.pathname.includes("/cgi");
+  const libraryToDisplay = isCgiPage ? cgiLibrary : videoLibrary;
+  const [selectedVideo, setSelectedVideo] = useState(isCgiPage ? cgiLibrary[0] : videoLibrary[0]);
   const videoRef = useRef(null);
+
 
   useEffect(() => {
     if (selectedVideo !== null) {
-      videoRef.current.src = selectedVideo.video;
+      videoRef.current = selectedVideo.video;
     }
   }, [selectedVideo]);
 
@@ -22,18 +29,19 @@ const VideoPlayer = () => {
           <div ref={videoRef}>
             <p>{selectedVideo.number}</p>
             <h2>{selectedVideo.title}</h2>
+            <img src={selectedVideo.thumbnail} alt={selectedVideo.title} />
             <iframe title={selectedVideo.title} src={selectedVideo.video} allowFullScreen />
           </div>
         )}
       </div>
-      {cgiLibrary.map((video) => (
+      {libraryToDisplay.map((video) => (
         <div onClick={() => handleClickVideo(video)} key={video.id} style={{ background: "red", margin: "20px 0" }}>
           <p>{video.number}</p>
           <h2>{video.title}</h2>
           <h3>{video.year} </h3>
           <p>{video.description}</p>
           <p>{video.software.join(" | ")} </p>
-          <iframe title={video.title} src={video.video} allowFullScreen />
+          <img src={video.thumbnail} alt={video.title} />
         </div>
       )
       )}
