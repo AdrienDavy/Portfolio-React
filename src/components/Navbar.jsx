f73cceafbeaeb065806d8f47bf2c9c7bff697c8d
Adrien Davy <adriendavyfx@gmail.com>
2023-12-16 11:33:20 +0100

    css navbar 1.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,29 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import Logo from '../assets/img/logo.svg';
+import LogoWhite from '../assets/img/logoWhite.svg';
 
 const Navbar = () => {
   return (
     <header>
-      <NavLink to="cgi">
+      <NavLink to="cgi" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
         CGI
+        <span></span>
       </NavLink>
-      <NavLink to="videos">
+      <NavLink to="videos" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
         Videos
+        <span></span>
       </NavLink>
-      <NavLink to="/">
-        Home
+      <NavLink to="/" className={({ isActive }) => (isActive ? "logo-clicked" : "logo-unclicked")} id="logoLink">
+        <img id="logo" src={Logo} alt="Logo" title="Accueil" />
+        <img id="logo-white" src={LogoWhite} alt="Logo white" title="Accueil" />
       </NavLink>
-      <NavLink to="devweb">
+      <NavLink to="devweb" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
         Dev Web
+        <span></span>
       </NavLink>
-      <NavLink to="contact">
+      <NavLink to="contact" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
         Contact
+        <span></span>
       </NavLink>
     </header>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import FormTemplate from "@components/FormTemplate"
+import background from '@videoSources/home.mp4';
 
 const Contact = () => {
   return (
-    <div>
+    <div className="contact-container">
       <FormTemplate />
+      <video src={background} autoPlay muted loop />
     </div>
   )
 }
