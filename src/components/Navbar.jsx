2c24b552338cbceb9dc3cd36a5a16db89f280248
Adrien Davy <adriendavyfx@gmail.com>
2023-12-18 13:04:04 +0100

    css navbar and responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,56 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import Logo from '../assets/img/logo.svg';
 import LogoWhite from '../assets/img/logoWhite.svg';
+import BurgerIcon from "../assets/img/icon-burger.svg";
+import CloseIcon from "../assets/img/icon-close.svg";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const toggleMenu = () => {
+
+    setIsOpen(!isOpen);
+  };
+
+
   return (
-    <header>
-      <NavLink to="cgi" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
-        CGI
-        <span></span>
-      </NavLink>
-      <NavLink to="videos" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
-        Videos
-        <span></span>
-      </NavLink>
-      <NavLink to="/" className={({ isActive }) => (isActive ? "logo-clicked" : "logo-unclicked")} id="logoLink">
-        <img id="logo" src={Logo} alt="Logo" title="Accueil" />
-        <img id="logo-white" src={LogoWhite} alt="Logo white" title="Accueil" />
-      </NavLink>
-      <NavLink to="devweb" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
-        Dev Web
-        <span></span>
-      </NavLink>
-      <NavLink to="contact" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
-        Contact
-        <span></span>
-      </NavLink>
-    </header>
+    <>
+      <button type="button" className="burger" onClick={toggleMenu}>
+        <img
+          src={isOpen ? CloseIcon : BurgerIcon}
+          alt="Burger icon and cross close icon"
+          className="burger-icon"
+        />
+      </button>
+      <div className={isOpen ? "background open" : "background"}></div>
+      <nav className={isOpen ? "navbar-container open" : "navbar-container"}>
+        <ul className="overlay">
+          <NavLink to="cgi" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
+            <li><p>CGI</p><span></span></li>
+
+          </NavLink>
+          <NavLink to="videos" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
+            <li><p>Videos</p><span></span></li>
+
+          </NavLink>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "logo-clicked" : "logo-unclicked")} id="logoLink">
+            <li> <img id="logo" src={Logo} alt="Logo" title="Accueil" />
+              <img id="logo-white" src={LogoWhite} alt="Logo white" title="Accueil" />
+            </li>
+          </NavLink>
+          <NavLink to="devweb" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
+            <li><p>Dev Web</p><span></span></li>
+
+          </NavLink>
+          <NavLink to="contact" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
+            <li><p>Contact</p><span></span></li>
+
+          </NavLink>
+        </ul>
+      </nav>
+    </>
   )
 }
 
