a23fd91394c55d238beff273e48754759c5fa685
Adrien Davy <adriendavyfx@gmail.com>
2023-12-20 21:49:50 +0100

    target style done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from '../assets/img/logo.svg';
 import LogoWhite from '../assets/img/logoWhite.svg';
 import BurgerIcon from "../assets/img/icon-burger.svg";
@@ -9,37 +9,96 @@ import { useVideo } from "@contexts/VideoContext";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { playAnimation } = useVideo();
-
+  const [activeButton, setActiveButton] = useState(null);
+  const [isAnimating, setIsAnimating] = useState(false);
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   const [targetStyle, setTargetStyle] = useState({
-    width: 0,
+    bottom: 0,
     height: 0,
-    top: 0,
     left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+    x: 0,
+    y: 0
   });
 
+  const handleButtonClick = async (event) => {
 
-
-
-
-  const handleButtonClick = (event) => {
     const targetButton = event.currentTarget;
-    const buttonRect = targetButton.getBoundingClientRect();
+    const buttonRect = await targetButton.getBoundingClientRect();
+    const defaultWidth = 10;
+    const defaultHeight = 10;
+    const defaultLeft = 5;
+    const defaultTop = 5;
 
     setTargetStyle({
-      width: `${buttonRect.width}px`,
-      height: `${buttonRect.height}px`,
-      top: `${buttonRect.top}px`,
-      left: `${buttonRect.left}px`,
+      bottom: `${buttonRect.bottom}px`,
+      height: `${buttonRect.height + defaultHeight}px`,
+      left: `${buttonRect.left - defaultLeft}px`,
+      right: `${buttonRect.right}px`,
+      top: `${buttonRect.top - defaultTop}px`,
+      width: `${buttonRect.width + defaultWidth}px`,
+      x: `${buttonRect.x}px`,
+      y: `${buttonRect.y}px`,
     });
-    console.log(buttonRect);
+
+    setActiveButton(targetButton);
+    setIsAnimating(true);
+
+    // Attendre la fin du mouvement (par exemple, après 300 ms)
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 60);
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Mettez à jour le style de l'élément animated-target en cas de redimensionnement de la fenêtre
+      if (activeButton) {
+        const buttonRect = activeButton.getBoundingClientRect();
+
+        setTargetStyle({
+          bottom: `${buttonRect.bottom}px`,
+          height: `${buttonRect.height}px`,
+          left: `${buttonRect.left}px`,
+          right: `${buttonRect.right}px`,
+          top: `${buttonRect.top}px`,
+          width: `${buttonRect.width}px`,
+          x: `${buttonRect.x}px`,
+          y: `${buttonRect.y}px`,
+        });
+      }
+    };
+
+    // Ajouter un écouteur d'événements pour le redimensionnement de la fenêtre
+    window.addEventListener('resize', handleResize);
+
+    // Supprimer l'écouteur d'événements lors du démontage du composant
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [activeButton]);
+
 
   return (
     <>
+      <div
+        className={playAnimation ? `animated-target ${isAnimating ? 'blur' : ''} hide` : `animated-target ${isAnimating ? 'blur' : ''}`}
+        style={targetStyle}
+      >
+        <span className="tlv"></span>
+        <span className="tlh"></span>
+        <span className="trv"></span>
+        <span className="trh"></span>
+        <span className="brv"></span>
+        <span className="brh"></span>
+        <span className="blv"></span>
+        <span className="blh"></span>
+      </div>
       <button type="button" className="burger" onClick={toggleMenu}>
         <img
           src={isOpen ? CloseIcon : BurgerIcon}
@@ -50,40 +109,34 @@ const Navbar = () => {
       <header className={playAnimation ? "header hide" : "header"}>
         <nav className={isOpen ? "navbar-container open" : "navbar-container"}>
           <ul className="overlay">
-            <div
-              className="animated-target"
-              style={targetStyle}
-            ></div>
 
-            <button className="btn-primary first" onClick={handleButtonClick}>
+            <button className="btn-primary first" onMouseOver={handleButtonClick}>
               <NavLink to="cgi" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
                 <li><p>CGI</p><span></span></li>
               </NavLink>
             </button>
 
-            <button className="btn-primary" onClick={handleButtonClick}>
+            <button className="btn-primary" onMouseOver={handleButtonClick}>
               <NavLink to="videos" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
                 <li><p>Videos</p><span></span></li>
-
               </NavLink>
             </button>
 
-            <NavLink to="/" className={({ isActive }) => (isActive ? "logo-clicked" : "logo-unclicked")} id="logoLink">
+            <NavLink to="/" className={({ isActive }) => (isActive ? "logo-clicked" : "logo-unclicked")} id="logoLink" onMouseOver={handleButtonClick}>
               <li> <img id="logo" src={Logo} alt="Logo" title="Accueil" />
                 <img id="logo-white" src={LogoWhite} alt="Logo white" title="Accueil" />
               </li>
             </NavLink>
 
-            <button className="btn-primary" onClick={handleButtonClick}>
+            <button className="btn-primary" onMouseOver={handleButtonClick}>
               <NavLink to="devweb" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
                 <li><p>Dev Web</p><span></span></li>
 
               </NavLink>
             </button>
-            <button className="btn-primary" onClick={handleButtonClick}>
+            <button className="btn-primary" onMouseOver={handleButtonClick}>
               <NavLink to="contact" className={({ isActive }) => (isActive ? "clicked" : "unclicked")}>
                 <li><p>Contact</p><span></span></li>
-
               </NavLink>
             </button>
           </ul>
