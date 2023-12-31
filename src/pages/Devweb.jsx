bd5955ee7599f637c4eb8ac1516e35d88c324a48
Adrien Davy <adriendavyfx@gmail.com>
2023-12-31 12:59:10 +0100

    css done

--- src/pages/Devweb.jsx
+++ src/pages/Devweb.jsx
@@ -7,32 +7,35 @@ const Devweb = () => {
     setSiteSelected(site);
   }
 
-  const containerStyle = {
-    background: `url(${siteSelected.thumbnail}) no-repeat center`,
-    backgroundSize: 'cover',
-  };
 
   return (
-    <div className="devweb-container" style={{ containerStyle }}>
+    <div className="devweb-container" style={{ background: `url(${siteSelected.thumbnail}) no-repeat center` }}>
       {siteSelected && (<div className="site-selection">
-
-        <p className="number">{siteSelected?.number}</p>
-        <h2>{siteSelected?.title}</h2>
-        <h4>{siteSelected?.year} </h4>
-        <p className="description">{siteSelected?.description} </p>
-        {siteSelected?.software ? <h3>{siteSelected.software.join(" | ")} </h3> : null}
-        {siteSelected?.langage ? <h3>{siteSelected.langage.join(" | ")} </h3> : null}
-        <a href={siteSelected?.link} target="_blank" rel="noreferrer"> <img src={siteSelected?.thumbnail} alt={siteSelected?.title} /></a>
+        <div className="text">
+          <p className="number">{siteSelected?.number}</p>
+          <h2>{siteSelected?.title}</h2>
+          <h4>{siteSelected?.year} </h4>
+          <p className="description">{siteSelected?.description} </p>
+          {siteSelected?.software ? <h3>{siteSelected.software.join(" | ")} </h3> : null}
+          {siteSelected?.langage ? <h3>{siteSelected.langage.join(" | ")} </h3> : null}
+        </div>
+        <div className="image-link">
+          <a href={siteSelected?.link} target="_blank" rel="noreferrer">
+            <img src={siteSelected?.thumbnail} alt={siteSelected?.title} />
+          </a>
+        </div>
+        <div className="website-list">
+          {devwebLibrary.map((website) => (
+            <div className="website" key={website.id} onClick={() => handleClickSite(website)} style={{ background: `url(${website.thumbnail}) no-repeat center`, backgroundSize: 'cover' }}>
+              <div className="filter-site">
+                <p className="number">{website?.number}</p>
+                <h2>{website?.title}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>)}
-      <div className="website-list">
-        {devwebLibrary.map((website) => (
-          <div className="website" key={website.id} onClick={() => handleClickSite(website)} style={{ background: `url(${website.thumbnail}) no-repeat center`, backgroundSize: 'cover' }}>
-            <p className="number">{website?.number}</p>
-            <div className="filter" ></div>
-            <h2>{website?.title}</h2>
-          </div>
-        ))}
-      </div>
+
     </div>
   )
 }
